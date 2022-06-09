c91d9220e5192aa2090a32d35341f5223200262b
Aniela Katana <anielakatana@MacBook-Pro-Aniela-2.local>
2022-06-09 20:03:25 +0200

    update config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  target: 'serverless',
   experimental: {
     // Enables the styled-components SWC transform
     styledComponents: true
